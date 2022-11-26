3aa9a4ad061c6c6586dad2abaafc4aeb235e383b
David Taing <adavidtaing@gmail.com>
2022-11-26 23:44:05 +1100

    build: add and setup @tanstack/react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 if (process.env.NODE_ENV !== "production") {
   require("../mocks");
 }
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
