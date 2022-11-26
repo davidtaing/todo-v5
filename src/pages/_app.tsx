31206538582d4744f445ce1d634187b83262ad29
David Taing <adavidtaing@gmail.com>
2022-11-27 00:40:40 +1100

    fix: Next App init before MSW mocking enabled

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,31 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient } from "@tanstack/react-query";
 import { QueryClientWrapper } from "../components/utils/QueryClientWrapper";
-
-if (process.env.NODE_ENV !== "production") {
-  require("../mocks");
-}
+import { useState, useEffect } from "react";
 
 const queryClient = new QueryClient();
 
+const API_MOCKING = process.env.NODE_ENV !== "production";
+
 export default function App({ Component, pageProps }: AppProps) {
+  const [shouldRender, setShouldRender] = useState(API_MOCKING);
+
+  useEffect(() => {
+    async function initMocks() {
+      const { initMocks } = await import("../mocks");
+      await initMocks();
+      setShouldRender(true);
+    }
+
+    if (API_MOCKING) {
+      initMocks();
+    }
+  }, []);
+
+  if (!shouldRender) {
+    return null;
+  }
+
   return (
     <QueryClientWrapper queryClient={queryClient}>
       <Component {...pageProps} />
