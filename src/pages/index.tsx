11dee76afd4c5cac3ef60f8425b13e3221d76080
David Taing <adavidtaing@gmail.com>
2022-11-26 23:12:38 +1100

    build: add and setup Mock Service Worker

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 // pages/index.js
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Home() {
   return (
     <nav>
       <Link href="/about">About</Link>
     </nav>
-  )
+  );
 }
