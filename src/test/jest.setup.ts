09fa9710b0055bdae55dc322ea02203a1768fc8a
David Taing <adavidtaing@gmail.com>
2022-11-28 19:55:11 +1100

    refactor: add next/router mock in Jest globalSetup
    due to nested components with useRouter throwing errors in tests.
    
    Error Message:
    Error: Uncaught [Error: Error: NextRouter was not mounted. https://nextjs.org/docs/messages/next-router-not-mounted]

--- src/test/jest.setup.ts
+++ src/test/jest.setup.ts
@@ -1,5 +1,7 @@
 import { server } from "../mocks/server";
+import { useRouter } from "next/router";
+jest.mock("next/router", () => require("next-router-mock"));
 
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
