11183dbbc377f1d33828cb0162f319adbeecfbba
David Taing <adavidtaing@gmail.com>
2022-12-04 23:00:35 +1100

    style Add Todo Input

--- src/features/Todo/components/AddTodo.tsx
+++ src/features/Todo/components/AddTodo.tsx
@@ -20,12 +20,18 @@ export const AddTodo = ({ onCreateTodo, disabled }: AddTodoProps) => {
   }, [disabled]);
 
   return (
-    <form aria-label="add-todo" onSubmit={onSubmit} method="POST">
+    <form
+      aria-label="add-todo"
+      onSubmit={onSubmit}
+      method="POST"
+      className="block p-4"
+    >
       <fieldset disabled={disabled}>
         <input
           placeholder="Add Todo"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
+          className="text-center w-full border border-slate-400 rounded py-2 px-3"
         />
       </fieldset>
     </form>
