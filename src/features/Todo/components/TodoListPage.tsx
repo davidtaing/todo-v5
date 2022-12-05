bcf019db109b107813c1cf5c5d1d37e32acd1646
David Taing <adavidtaing@gmail.com>
2022-12-05 12:03:00 +1100

    refactor: query and mutation logic to custom hook
    Changes:
    - refactor Todo query and mutations to useTodoRequests hook.

--- src/features/Todo/components/TodoListPage.tsx
+++ src/features/Todo/components/TodoListPage.tsx
@@ -1,143 +1,21 @@
-import { useRef } from "react";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-
 import { TodoList } from "./TodoList";
 
-import {
-  deleteTodo,
-  getTodos,
-  updateTodo,
-  createTodo,
-  CreateTodoSchema,
-} from "../api";
-import { GetTodoResponse, Todo } from "../../../types";
+import { Todo } from "../../../types";
 import { TodoItem } from "./TodoItem/TodoItem";
 import { AddTodo } from "./AddTodo";
+import { useTodoRequests } from "../hooks/useTodoRequests";
 
 export const TodoListPage = () => {
-  const dedupeCreateRequest = useRef(false);
-  const queryClient = useQueryClient();
-  const getQuery = useQuery({
-    queryKey: ["todos"],
-    queryFn: getTodos,
-  });
-  const todos = getQuery.data?.todos;
+  const {
+    todos,
+    getTodoQuery,
+    onDeleteTodo,
+    onToggleClick,
+    onCreateTodo,
+    dedupeCreateRequest,
+  } = useTodoRequests();
 
-  const deleteTodoMutation = useMutation({
-    mutationFn: deleteTodo,
-    onMutate: async (id: string) => {
-      await queryClient.cancelQueries(["todos"]);
-
-      const previousTodos = queryClient.getQueryData<GetTodoResponse>([
-        "todos",
-      ]);
-
-      if (previousTodos) {
-        queryClient.setQueryData<GetTodoResponse>(["todos"], {
-          todos: todos.filter((item: Todo) => item.id !== id),
-        });
-      }
-
-      return { previousTodos };
-    },
-    onError: (err, variables, context) => {
-      if (context?.previousTodos) {
-        queryClient.setQueryData<GetTodoResponse>(
-          ["todos"],
-          context.previousTodos
-        );
-      }
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries(["todos"]);
-    },
-  });
-
-  const updateTodoMutation = useMutation({
-    mutationFn: updateTodo,
-    onMutate: async (todo: Todo) => {
-      await queryClient.cancelQueries(["todos"]);
-
-      const previousTodos = queryClient.getQueryData<GetTodoResponse>([
-        "todos",
-      ]);
-
-      if (previousTodos) {
-        queryClient.setQueryData<GetTodoResponse>(["todos"], {
-          todos: todos.map((item: Todo) => {
-            if (item.id === todo.id) {
-              return todo;
-            }
-
-            return item;
-          }),
-        });
-      }
-
-      return { previousTodos };
-    },
-    onError: (err, variables, context) => {
-      if (context?.previousTodos) {
-        queryClient.setQueryData<GetTodoResponse>(
-          ["todos"],
-          context.previousTodos
-        );
-      }
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries(["todos"]);
-    },
-  });
-
-  const createTodoMutation = useMutation({
-    mutationFn: createTodo,
-    onMutate: async (createTodoRequestBody: CreateTodoSchema) => {
-      await queryClient.cancelQueries(["todos"]);
-      dedupeCreateRequest.current = true;
-
-      const previousTodos = queryClient.getQueryData<GetTodoResponse>([
-        "todos",
-      ]);
-
-      const newTodo = {
-        id: "",
-        userId: "",
-        completed: false,
-        create: Date.now(),
-        ...createTodoRequestBody,
-      };
-
-      if (previousTodos) {
-        queryClient.setQueryData<GetTodoResponse>(["todos"], {
-          todos: [...todos, newTodo],
-        });
-      }
-
-      return { previousTodos };
-    },
-    onError: (err, variables, context) => {
-      if (context?.previousTodos) {
-        queryClient.setQueryData<GetTodoResponse>(
-          ["todos"],
-          context.previousTodos
-        );
-      }
-    },
-    onSettled: () => {
-      dedupeCreateRequest.current = false;
-      queryClient.invalidateQueries(["todos"]);
-    },
-  });
-
-  const onDeleteTodo = (id: string) => deleteTodoMutation.mutate(id);
-  const onToggleClick = (todo: Todo) =>
-    updateTodoMutation.mutate({ ...todo, completed: !todo.completed });
-  const onCreateTodo = (createTodoRequestBody: CreateTodoSchema) => {
-    if (dedupeCreateRequest.current === false)
-      createTodoMutation.mutate(createTodoRequestBody);
-  };
-
-  if (getQuery.isLoading) {
+  if (getTodoQuery.isLoading) {
     return (
       <div aria-label="todo-list page">
         <div>Loading</div>
---
+++ src/features/Todo/hooks/useTodoRequests.tsx
@@ -0,0 +1,144 @@
+import { useRef } from "react";
+import { useQueryClient, useQuery, useMutation } from "@tanstack/react-query";
+
+import {
+  getTodos,
+  deleteTodo,
+  updateTodo,
+  createTodo,
+  CreateTodoSchema,
+} from "../api";
+import { GetTodoResponse, Todo } from "../../../types";
+
+export const useTodoRequests = () => {
+  const dedupeCreateRequest = useRef(false);
+  const queryClient = useQueryClient();
+  const getTodoQuery = useQuery({
+    queryKey: ["todos"],
+    queryFn: getTodos,
+  });
+  const todos = getTodoQuery.data?.todos;
+
+  const deleteTodoMutation = useMutation({
+    mutationFn: deleteTodo,
+    onMutate: async (id: string) => {
+      await queryClient.cancelQueries(["todos"]);
+
+      const previousTodos = queryClient.getQueryData<GetTodoResponse>([
+        "todos",
+      ]);
+
+      if (previousTodos) {
+        queryClient.setQueryData<GetTodoResponse>(["todos"], {
+          todos: todos.filter((item: Todo) => item.id !== id),
+        });
+      }
+
+      return { previousTodos };
+    },
+    onError: (err, variables, context) => {
+      if (context?.previousTodos) {
+        queryClient.setQueryData<GetTodoResponse>(
+          ["todos"],
+          context.previousTodos
+        );
+      }
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(["todos"]);
+    },
+  });
+
+  const updateTodoMutation = useMutation({
+    mutationFn: updateTodo,
+    onMutate: async (todo: Todo) => {
+      await queryClient.cancelQueries(["todos"]);
+
+      const previousTodos = queryClient.getQueryData<GetTodoResponse>([
+        "todos",
+      ]);
+
+      if (previousTodos) {
+        queryClient.setQueryData<GetTodoResponse>(["todos"], {
+          todos: todos.map((item: Todo) => {
+            if (item.id === todo.id) {
+              return todo;
+            }
+
+            return item;
+          }),
+        });
+      }
+
+      return { previousTodos };
+    },
+    onError: (err, variables, context) => {
+      if (context?.previousTodos) {
+        queryClient.setQueryData<GetTodoResponse>(
+          ["todos"],
+          context.previousTodos
+        );
+      }
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(["todos"]);
+    },
+  });
+
+  const createTodoMutation = useMutation({
+    mutationFn: createTodo,
+    onMutate: async (createTodoRequestBody: CreateTodoSchema) => {
+      await queryClient.cancelQueries(["todos"]);
+      dedupeCreateRequest.current = true;
+
+      const previousTodos = queryClient.getQueryData<GetTodoResponse>([
+        "todos",
+      ]);
+
+      const newTodo = {
+        id: "",
+        userId: "",
+        completed: false,
+        create: Date.now(),
+        ...createTodoRequestBody,
+      };
+
+      if (previousTodos) {
+        queryClient.setQueryData<GetTodoResponse>(["todos"], {
+          todos: [...todos, newTodo],
+        });
+      }
+
+      return { previousTodos };
+    },
+    onError: (err, variables, context) => {
+      if (context?.previousTodos) {
+        queryClient.setQueryData<GetTodoResponse>(
+          ["todos"],
+          context.previousTodos
+        );
+      }
+    },
+    onSettled: () => {
+      dedupeCreateRequest.current = false;
+      queryClient.invalidateQueries(["todos"]);
+    },
+  });
+
+  const onDeleteTodo = (id: string) => deleteTodoMutation.mutate(id);
+  const onToggleClick = (todo: Todo) =>
+    updateTodoMutation.mutate({ ...todo, completed: !todo.completed });
+  const onCreateTodo = (createTodoRequestBody: CreateTodoSchema) => {
+    if (dedupeCreateRequest.current === false)
+      createTodoMutation.mutate(createTodoRequestBody);
+  };
+
+  return {
+    todos,
+    getTodoQuery,
+    onDeleteTodo,
+    onToggleClick,
+    onCreateTodo,
+    dedupeCreateRequest,
+  };
+};
