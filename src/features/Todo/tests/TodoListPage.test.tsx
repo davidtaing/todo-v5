97ea8cd522f04d47233b60e1aec888dcf36d6023
David Taing <adavidtaing@gmail.com>
2022-12-07 20:06:50 +1100

    refactor: rearrange and rename tests
    for better readability.
    
    Changes:
    - group related test by category
    - rename tests using less technical language

--- src/features/Todo/tests/TodoListPage.test.tsx
+++ src/features/Todo/tests/TodoListPage.test.tsx
@@ -19,240 +19,248 @@ describe("TodoListPage", () => {
     expect(screen.queryByLabelText(/todo-list page/i)).toBeTruthy();
   });
 
-  it("renders a list of items when it fetches todos", async () => {
-    server.use(
-      rest.get("http://localhost:3000/api/todos", async (req, res, ctx) => {
-        const todos: Todo[] = [
-          {
-            id: "1",
-            userId: "1",
-            title: "Sleep Early",
-            completed: false,
-            created: new Date(),
-          },
-          {
-            id: "2",
-            userId: "1",
-            title: "Wake Up Early",
-            completed: false,
-            created: new Date(),
-          },
-        ];
+  describe("reading todos", () => {
+    it("renders a list of todos", async () => {
+      server.use(
+        rest.get("http://localhost:3000/api/todos", async (req, res, ctx) => {
+          const todos: Todo[] = [
+            {
+              id: "1",
+              userId: "1",
+              title: "Sleep Early",
+              completed: false,
+              created: new Date(),
+            },
+            {
+              id: "2",
+              userId: "1",
+              title: "Wake Up Early",
+              completed: false,
+              created: new Date(),
+            },
+          ];
 
-        return res(
-          ctx.status(200),
-          ctx.json({
-            todos,
-          })
-        );
-      })
-    );
+          return res(
+            ctx.status(200),
+            ctx.json({
+              todos,
+            })
+          );
+        })
+      );
 
-    const container = render(<TodoListPage />, {
-      wrapper: TestQueryClientWrapper,
+      const container = render(<TodoListPage />, {
+        wrapper: TestQueryClientWrapper,
+      });
+
+      await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
+
+      const listItems = container.queryAllByRole("listitem", {
+        name: /todo-item/i,
+      });
+
+      expect(listItems.length).toBe(2);
     });
-
-    await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
-
-    const listItems = container.queryAllByRole("listitem", {
-      name: /todo-item/i,
-    });
-
-    expect(listItems.length).toBe(2);
   });
 
-  test("deletes the Todo when the DeleteTodoButton is clicked", async () => {
-    server.use(
-      rest.get("http://localhost:3000/api/todos", async (req, res, ctx) => {
-        const todos: Todo[] = [
-          {
-            id: "1",
-            userId: "1",
-            title: "Sleep Early",
-            completed: true,
-            created: new Date(),
-          },
-        ];
+  describe("deleting todos", () => {
+    it("optimistically deletes the Todo", async () => {
+      server.use(
+        rest.get("http://localhost:3000/api/todos", async (req, res, ctx) => {
+          const todos: Todo[] = [
+            {
+              id: "1",
+              userId: "1",
+              title: "Sleep Early",
+              completed: true,
+              created: new Date(),
+            },
+          ];
 
-        return res(
-          ctx.status(200),
-          ctx.json({
-            todos,
-          })
-        );
-      })
-    );
+          return res(
+            ctx.status(200),
+            ctx.json({
+              todos,
+            })
+          );
+        })
+      );
 
-    const user = userEvent.setup();
+      const user = userEvent.setup();
 
-    render(<TodoListPage />, { wrapper: TestQueryClientWrapper });
+      render(<TodoListPage />, { wrapper: TestQueryClientWrapper });
 
-    await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
+      await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
 
-    const beforeTodoItems = screen.queryAllByRole("listitem", {
-      name: /todo-item/i,
+      const beforeTodoItems = screen.queryAllByRole("listitem", {
+        name: /todo-item/i,
+      });
+
+      expect(beforeTodoItems).toHaveLength(1);
+
+      const deleteTodoButton = screen.getByRole("button", { name: "❌" });
+
+      await user.click(deleteTodoButton);
+
+      const afterTodoItems = screen.queryAllByRole("listitem", {
+        name: /todo-item/i,
+      });
+
+      expect(afterTodoItems).toHaveLength(0);
     });
-
-    expect(beforeTodoItems).toHaveLength(1);
-
-    const deleteTodoButton = screen.getByRole("button", { name: "❌" });
-
-    await user.click(deleteTodoButton);
-
-    const afterTodoItems = screen.queryAllByRole("listitem", {
-      name: /todo-item/i,
-    });
-
-    expect(afterTodoItems).toHaveLength(0);
   });
 
-  test("toggling a Todo to 'complete' causes DeleteTodoButton to render", async () => {
-    server.use(
-      rest.get("http://localhost:3000/api/todos", async (req, res, ctx) => {
-        const todos: Todo[] = [
-          {
-            id: "1",
-            userId: "1",
-            title: "Sleep Early",
-            completed: false,
-            created: new Date(),
-          },
-        ];
+  describe("creating todos", () => {
+    it("optimistically creates a todo", async () => {
+      server.use(
+        rest.get("http://localhost:3000/api/todos", async (req, res, ctx) => {
+          const todos: Todo[] = [];
 
-        return res(
-          ctx.status(200),
-          ctx.json({
-            todos,
-          })
-        );
-      })
-    );
+          return res(
+            ctx.status(200),
+            ctx.json({
+              todos,
+            })
+          );
+        })
+      );
 
-    const user = userEvent.setup();
+      const user = userEvent.setup();
 
-    render(<TodoListPage />, { wrapper: TestQueryClientWrapper });
+      render(<TodoListPage />, { wrapper: TestQueryClientWrapper });
 
-    await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
+      await waitForElementToBeRemoved(() => screen.getByText(/^loading/i));
 
-    expect(screen.queryByRole("button", { name: "❌" })).toBeFalsy();
+      const addTodoInput = screen.getByPlaceholderText(/^new todo/i);
 
-    const toggle = screen.getByRole("checkbox");
+      await user.click(addTodoInput);
+      await user.keyboard("test todo");
+      await user.keyboard("[enter]");
 
-    await user.click(toggle);
+      const todoItem = screen.queryByRole("listitem", {
+        name: /todo-item/i,
+      });
 
-    expect(screen.queryByRole("button", { name: "❌" })).toBeTruthy();
-  });
-
-  test("toggling a Todo to 'incomplete' causes DeleteTodoButton to disappear", async () => {
-    server.use(
-      rest.get("http://localhost:3000/api/todos", async (req, res, ctx) => {
-        const todos: Todo[] = [
-          {
-            id: "1",
-            userId: "1",
-            title: "Sleep Early",
-            completed: true,
-            created: new Date(),
-          },
-        ];
-
-        return res(
-          ctx.status(200),
-          ctx.json({
-            todos,
-          })
-        );
-      })
-    );
-
-    const user = userEvent.setup();
-
-    render(<TodoListPage />, { wrapper: TestQueryClientWrapper });
-
-    await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
-
-    expect(screen.queryByRole("button", { name: "❌" })).toBeTruthy();
-
-    const toggle = screen.getByRole("checkbox");
-
-    await user.click(toggle);
-
-    expect(screen.queryByRole("button", { name: "❌" })).toBeFalsy();
-  });
-
-  test("create a new todo", async () => {
-    server.use(
-      rest.get("http://localhost:3000/api/todos", async (req, res, ctx) => {
-        const todos: Todo[] = [];
-
-        return res(
-          ctx.status(200),
-          ctx.json({
-            todos,
-          })
-        );
-      })
-    );
-
-    const user = userEvent.setup();
-
-    render(<TodoListPage />, { wrapper: TestQueryClientWrapper });
-
-    await waitForElementToBeRemoved(() => screen.getByText(/^loading/i));
-
-    const addTodoInput = screen.getByPlaceholderText(/^new todo/i);
-
-    await user.click(addTodoInput);
-    await user.keyboard("test todo");
-    await user.keyboard("[enter]");
-
-    const todoItem = screen.queryByRole("listitem", {
-      name: /todo-item/i,
+      expect(todoItem).toBeTruthy();
     });
 
-    expect(todoItem).toBeTruthy();
+    it("prevents creating another todo when already creating a todo", async () => {
+      server.use(
+        rest.get("http://localhost:3000/api/todos", async (req, res, ctx) => {
+          const todos: Todo[] = [];
+
+          return res(
+            ctx.status(200),
+            ctx.json({
+              todos,
+            })
+          );
+        })
+      );
+
+      const user = userEvent.setup();
+
+      render(<TodoListPage />, { wrapper: TestQueryClientWrapper });
+
+      await waitForElementToBeRemoved(() => screen.getByText(/^loading/i));
+
+      const addTodoInput = screen.getByPlaceholderText(/^new todo/i);
+
+      await user.click(addTodoInput);
+      await user.keyboard("test todo");
+      await user.keyboard("[enter]");
+
+      const todoItemsBefore = screen.queryByRole("listitem", {
+        name: /todo-item/i,
+      });
+
+      expect(todoItemsBefore).toBeTruthy();
+
+      await user.click(addTodoInput);
+      await user.keyboard("test todo");
+      await user.keyboard("[enter]");
+
+      const todoItemsAfter = screen.queryAllByRole("listitem", {
+        name: /todo-item/i,
+      });
+
+      expect(todoItemsAfter).toHaveLength(1);
+    });
   });
 
-  it("prevents creating another todo when the createTodoMutation is running", async () => {
-    server.use(
-      rest.get("http://localhost:3000/api/todos", async (req, res, ctx) => {
-        const todos: Todo[] = [];
+  describe("updating todos", () => {
+    it("renders the Delete button when toggled to 'complete'", async () => {
+      server.use(
+        rest.get("http://localhost:3000/api/todos", async (req, res, ctx) => {
+          const todos: Todo[] = [
+            {
+              id: "1",
+              userId: "1",
+              title: "Sleep Early",
+              completed: false,
+              created: new Date(),
+            },
+          ];
 
-        return res(
-          ctx.status(200),
-          ctx.json({
-            todos,
-          })
-        );
-      })
-    );
+          return res(
+            ctx.status(200),
+            ctx.json({
+              todos,
+            })
+          );
+        })
+      );
 
-    const user = userEvent.setup();
+      const user = userEvent.setup();
 
-    render(<TodoListPage />, { wrapper: TestQueryClientWrapper });
+      render(<TodoListPage />, { wrapper: TestQueryClientWrapper });
 
-    await waitForElementToBeRemoved(() => screen.getByText(/^loading/i));
+      await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
 
-    const addTodoInput = screen.getByPlaceholderText(/^new todo/i);
+      expect(screen.queryByRole("button", { name: "❌" })).toBeFalsy();
 
-    await user.click(addTodoInput);
-    await user.keyboard("test todo");
-    await user.keyboard("[enter]");
+      const toggle = screen.getByRole("checkbox");
 
-    const todoItemsBefore = screen.queryByRole("listitem", {
-      name: /todo-item/i,
+      await user.click(toggle);
+
+      expect(screen.queryByRole("button", { name: "❌" })).toBeTruthy();
     });
 
-    expect(todoItemsBefore).toBeTruthy();
+    it("removes the Delete button when toggled to 'incomplete'", async () => {
+      server.use(
+        rest.get("http://localhost:3000/api/todos", async (req, res, ctx) => {
+          const todos: Todo[] = [
+            {
+              id: "1",
+              userId: "1",
+              title: "Sleep Early",
+              completed: true,
+              created: new Date(),
+            },
+          ];
 
-    await user.click(addTodoInput);
-    await user.keyboard("test todo");
-    await user.keyboard("[enter]");
+          return res(
+            ctx.status(200),
+            ctx.json({
+              todos,
+            })
+          );
+        })
+      );
 
-    const todoItemsAfter = screen.queryAllByRole("listitem", {
-      name: /todo-item/i,
+      const user = userEvent.setup();
+
+      render(<TodoListPage />, { wrapper: TestQueryClientWrapper });
+
+      await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
+
+      expect(screen.queryByRole("button", { name: "❌" })).toBeTruthy();
+
+      const toggle = screen.getByRole("checkbox");
+
+      await user.click(toggle);
+
+      expect(screen.queryByRole("button", { name: "❌" })).toBeFalsy();
     });
-
-    expect(todoItemsAfter).toHaveLength(1);
   });
 });
